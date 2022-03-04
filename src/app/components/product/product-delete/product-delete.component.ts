9b7134715cfce7b2bbe839c18c27c44e46a6eae9
Johann Arouca <aroucajohann@yahoo.com>
2022-03-04 19:05:07 -0300

    04/03

---
+++ src/app/components/product/product-delete/product-delete.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../product.model';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-product-delete',
+  templateUrl: './product-delete.component.html',
+  styleUrls: ['./product-delete.component.css']
+})
+export class ProductDeleteComponent implements OnInit {
+
+  product: Product = {
+    name: '',
+    price: 0
+  }
+
+  constructor(private productService: ProductService, private router: Router,  private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    if (id === null) {
+      throw Error
+    }
+    this.productService.readById(id).subscribe(product => {
+      this.product = product
+    })
+  }
+
+  deleteProduct(): void {
+    if (this.product.id === undefined) {
+      throw Error
+    }
+    this.productService.delete(this.product.id).subscribe(() => {
+      this.productService.showMessage('Produco excluído com sucesso!')
+      this.router.navigate(['/products'])
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
+}
